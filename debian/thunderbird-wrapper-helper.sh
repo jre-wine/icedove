698f966086d78fe2a7abf5ee50c5904b8e22b573
Carsten Schoenert <c.schoenert@t-online.de>
2017-02-27 13:15:17 +0100

    squash me into previous commit

--- debian/thunderbird-wrapper-helper.sh
+++ debian/thunderbird-wrapper-helper.sh
@@ -40,14 +40,27 @@ The following error happened:"
 DOT_THUNDERBIRD_EXISTS="\
 ${DEFAULT_X11_MSG}
 
-An existing profile folder '.thunderbird' was found in your Home
-directory '${HOME}/' while trying to migrate the Icedove
+An existing profile folder (or symlink) '.thunderbird' was found in
+your Home directory '${HOME}/' while trying to migrate the Icedove
 profile(s) folder!
 
 This can probably be a old, currently not used profile folder or
 you maybe using a Thunderbird installation from the Mozilla packages.
 If you don't need this old profile folder, you can remove or backup
 it and start Thunderbird again.
+
+Sorry, but please investigate the situation by yourself.
+
+Please mind also the information in section 'Profile Migration'
+given in the file
+
+/usr/share/doc/thunderbird/README.Debian.gz
+"
+THUNDERBIRD_PROFILE_LINKING_ERROR="\
+${DEFAULT_X11_MSG}
+
+A needed symlink for the Thunderbird profile(s) folder '.thunderbird'
+to the old existing Icedove profile '.icedove' couldn't created. 
 
 Sorry, but please investigate the situation by yourself.
 
@@ -80,23 +93,30 @@ TITLE="Icedove to Thunderbird Profile adoption"
 # local functions #
 ###################
 
-# Simple debugging function
-debug () {
-if [ "${VERBOSE}" = "1" ]; then
-    echo "DEBUG -> $1"
+# Simple search all files where we made a backup from
+do_collect_backup_files () {
+output_debug "Collect all files we've made a backup."
+BACKUP_FILES=$(/usr/bin/find ${TB_PROFILE_FOLDER}/ -type f -name "*backup_thunderbird_migration*")
+if [ "${BACKUP_FILES}" != "" ]; then
+    output_info "The following backups related Icedove to Thunderbird transition are existing:"
+    /usr/bin/cat << EOF
+${BACKUP_FILES}
+EOF
+else
+    output_info "No backups related Icedove to Thunderbird transition found."
 fi
 }
 
-# Simple search all files where we made a backup from
-do_collect_backup_files () {
-debug "Collect all files we've made a backup."
-BACKUP_FILES=$(/usr/bin/find ${HOME} -type f -name "*backup_thunderbird_migration2*")
-if [ "${BACKUP_FILES}" != "" ]; then
-    echo "The following backups related Icedove to Thunderbird transition are existing:\\n
-${BACKUP_FILES}"
-else
-    echo "No backups related Icedove to Thunderbird transition found."
-fi
+# Create the file .thunderbird/.migrated with some content
+do_create_migrated_mark_file (){
+/bin/cat <<EOF > ${TB_PROFILE_FOLDER}/.migrated
+This is a automatically created file by /usr/bin/thunderbird, it will be
+recreated by every start of Thunderbird. Remove that files only if you know
+the propose of this file.
+
+/usr/share/doc/thunderbird/README.Debian.gz will hold some useful information
+about this dot file.
+EOF
 }
 
 # Fix the file $PROFILE/mimeTypes.rdf
@@ -104,15 +124,14 @@ do_fix_mimetypes_rdf (){
 for MIME_TYPES_RDF_FILE in $(/usr/bin/find ${TB_PROFILE_FOLDER}/ -name mimeTypes.rdf); do
     RDF_SEARCH_PATTERN=$(grep '/usr/bin/iceweasel\|icedove' ${MIME_TYPES_RDF_FILE})
     if [ "${RDF_SEARCH_PATTERN}" != "" ]; then
-        echo ${RDF_SEARCH_PATTERN}
-        debug "Backup ${MIME_TYPES_RDF_FILE} to ${MIME_TYPES_RDF_FILE}.backup_thunderbird_migration-${DATE}"
-        cp ${MIME_TYPES_RDF_FILE} ${MIME_TYPES_RDF_FILE}.backup_thunderbird_migration-${DATE}
+        output_debug "Backup ${MIME_TYPES_RDF_FILE} to ${MIME_TYPES_RDF_FILE}.backup_thunderbird_migration-${DATE}"
+        cp "${MIME_TYPES_RDF_FILE}" "${MIME_TYPES_RDF_FILE}.backup_thunderbird_migration-${DATE}"
 
-        debug "Fixing possible broken 'mimeTypes.rdf'."
+        output_debug "Fixing possible broken 'mimeTypes.rdf'."
         sed -i "s|/usr/bin/iceweasel|/usr/bin/x-www-browser|g" "${MIME_TYPES_RDF_FILE}"
         sed -i "s|icedove|thunderbird|g" "${MIME_TYPES_RDF_FILE}"
     else
-        echo "No fix up for ${MIME_TYPES_RDF_FILE} needed."
+        output_info "No fix up for ${MIME_TYPES_RDF_FILE} needed."
     fi
 done
 }
@@ -149,32 +168,45 @@ do_migrate_old_icedove_desktop() {
 #   ~/.local/share/applications/
 #
 # icedove.desktop files are now deprecated, but still commonly around.
-# We can remove them.
-
+# We normally could remove them, but for safety only modify the files.
 # These mimeapps.list files configures default applications for MIME types.
-for MIMEAPPS_LIST in ${HOME}/.config/mimeapps.list ${HOME}/.local/share/applications/mimeapps.list; do
-    # Check if file exists and has old icedove entry
-    if [ -e "${MIMEAPPS_LIST}" ] && \
-          grep -iq "\(userapp-\)*icedove\(-.*\)*\.desktop" "${MIMEAPPS_LIST}"; then
-        debug "Fixing broken '${MIMEAPPS_LIST}'."
-        MIMEAPPS_LIST_COPY="${MIMEAPPS_LIST}.backup_thunderbird_migration-${DATE}"
 
-        # Fix mimeapps.list and create backup, but it's really unlikely we
-        # have an existing backup so no further checking here!
-        # (requires GNU sed 3.02 or ssed for case-insensitive "I")
-        sed -i.backup_thunderbird_migration-${DATE} "s|\(userapp-\)*icedove\(-.*\)*\.desktop|thunderbird.desktop|gI" "${MIMEAPPS_LIST}"
-        if [ $? -ne 0 ]; then
-            echo "The configuration file for default applications for some MIME types"
-            echo "'${MIMEAPPS_LIST}' couldn't be fixed."
-            echo "Please check for potential problems like low disk space or wrong access rights!"
-            logger -i -p warning -s "$0: [profile migration] Couldn't fix '${MIMEAPPS_LIST}'!"
-            exit 1
-        else
-            debug "A copy of the configuration file of default applications for some MIME types"
-            debug "was saved into '${MIMEAPPS_LIST_COPY}'."
-        fi
+# Only jump in loop if we haven't already done a migration before or the
+# user is forcing this by the option '--fixmime'.
+if [ ! -f ${TB_PROFILE_FOLDER}/.migrated -o "${FORCE_MIMEAPPS_MIGRATE}" = "1" ]; then
+    if [ ! -f "${TB_PROFILE_FOLDER}/.migrated" ]; then
+        output_debug "No migration mark '${TB_PROFILE_FOLDER}/.migrated' found, checking mimeapps.list files for possible migration."
+    elif [ "${FORCE_MIMEAPPS_MIGRATE}" = "1" ]; then
+        output_debug "Migration enforced by user! Checking mimeapps.list files once again for possible migration."
     fi
-done
+    for MIMEAPPS_LIST in ${HOME}/.config/mimeapps.list ${HOME}/.local/share/applications/mimeapps.list; do
+        # Check if file exists and has old icedove entry
+        if [ -e "${MIMEAPPS_LIST}" ] && \
+              grep -iq "\(userapp-\)*icedove\(-.*\)*\.desktop" "${MIMEAPPS_LIST}"; then
+
+            output_debug "Fixing broken '${MIMEAPPS_LIST}'."
+            MIMEAPPS_LIST_COPY="${MIMEAPPS_LIST}.backup_thunderbird_migration-${DATE}"
+
+            # Fix mimeapps.list and create a backup, but it's really unlikely we
+            # have an existing backup so no further checking here!
+            # (requires GNU sed 3.02 or ssed for case-insensitive "I")
+            sed -i.backup_thunderbird_migration-${DATE} "s|\(userapp-\)*icedove\(-.*\)*\.desktop|thunderbird.desktop|gI" "${MIMEAPPS_LIST}"
+            if [ $? -ne 0 ]; then
+                output_info "The configuration file for default applications for some MIME types"
+                output_info "'${MIMEAPPS_LIST}' couldn't be fixed."
+                output_info "Please check for potential problems like low disk space or wrong access rights!"
+                logger -i -p warning -s "$0: [profile migration] Couldn't fix '${MIMEAPPS_LIST}'!"
+                exit 1
+            else
+                output_debug "A copy of the configuration file of default applications for some MIME types"
+                output_debug "was saved into '${MIMEAPPS_LIST_COPY}'."
+            fi
+        else
+            output_info "No fix up for ${MIMEAPPS_LIST} needed."
+        fi
+        do_create_migrated_mark_file
+    done
+fi
 
 # Migrate old user specific desktop entries
 # Users could have always been created own desktop shortcuts for Icedove in
@@ -188,17 +220,19 @@ done
 # about available applications.
 
 for ICEDOVE_DESKTOP in $(find ${HOME}/.local/share/applications/ -iname "*icedove*.desktop"); do
+    output_debug "Backup ${ICEDOVE_DESKTOP} to ${ICEDOVE_DESKTOP}.backup_thunderbird_migration-${DATE}"
     ICEDOVE_DESKTOP_COPY=${ICEDOVE_DESKTOP}.backup_thunderbird_migration-${DATE}
-    mv ${ICEDOVE_DESKTOP} ${ICEDOVE_DESKTOP_COPY}
+    mv "${ICEDOVE_DESKTOP}" "${ICEDOVE_DESKTOP_COPY}"
     # Update the mimeinfo cache.
     # Not existing *.desktop files in there should simply be ignored by the system anyway.
     if [ -x "$(which update-desktop-database)" ]; then
-        update-desktop-database ${HOME}/.local/share/applications/
+        output_debug "Call 'update-desktop-database' to update the mimeinfo cache."
+        update-desktop-database "${HOME}/.local/share/applications/"
     fi
 done
 }
 
-# Print out a error message about not possible adoptions
+# Print out a error message about not possible adoption
 do_thunderbird2icedove_error_out (){
 case "${DESKTOP}" in
     gnome|mate|xfce)
@@ -224,10 +258,30 @@ esac
 
 # Symlink .thunderbird to .icedove
 do_thunderbird2icedove_symlink () {
-debug "Try to symlink '${TB_PROFILE_FOLDER}' to '${ID_PROFILE_FOLDER}'"
+output_debug "Try to symlink '${TB_PROFILE_FOLDER}' to '${ID_PROFILE_FOLDER}'"
 if /usr/bin/ln -s "${ID_PROFILE_FOLDER}" "${TB_PROFILE_FOLDER}"; then
     return 0
 else
+    case "${DESKTOP}" in
+        gnome|mate|xfce)
+            local_zenity --info --no-wrap --title "${TITLE}" --text "${THUNDERBIRD_PROFILE_LINKING_ERROR}"
+            if [ $? -ne 0 ]; then
+                local_xmessage -center "${THUNDERBIRD_PROFILE_LINKING_ERROR}"
+            fi
+            FAIL=1
+            ;;
+        kde)
+            local_kdialog --title "${TITLE}" --msgbox "${THUNDERBIRD_PROFILE_LINKING_ERROR}"
+            if [ $? -ne 0 ]; then
+                local_xmessage -center "${THUNDERBIRD_PROFILE_LINKING_ERROR}"
+            fi
+            FAIL=1
+            ;;
+        *)
+            xmessage -center "${THUNDERBIRD_PROFILE_LINKING_ERROR}"
+            FAIL=1
+            ;;
+    esac
     return 1
 fi
 }
@@ -248,7 +302,7 @@ if [ -f /usr/bin/xmessage ]; then
     /usr/bin/xmessage "$@"
 else
     # this should never be reached as thunderbird has a dependency on x11-utils!
-    echo "xmessage not found"
+    output_info "xmessage not found"
 fi
 }
 
@@ -259,6 +313,18 @@ if [ -f /usr/bin/zenity ]; then
     return 0
 else
     return 1
+fi
+}
+
+# Simple info output function
+output_info () {
+/usr/bin/echo "INFO  -> $1"
+}
+
+# Simple debugging output function
+output_debug () {
+if [ "${VERBOSE}" = "1" ]; then
+    /usr/bin/echo "DEBUG -> $1"
 fi
 }
 
--- debian/thunderbird-wrapper.sh
+++ debian/thunderbird-wrapper.sh
@@ -6,14 +6,14 @@
 #
 # Purpose:
 #   This is a wrapper script for starting the thunderbird binary with taking
-#   care of the searching for a old user Icedove profile folder and copying
+#   care of the searching for a old user Icedove profile folder and adopting
 #   the folder into the new place if possible.
 #
 # Environment:
 #   The Icedove binary was using the profile folder '${HOME}/.icedove'. The
 #   Mozilla default for the Thunderbird binary is '${HOME}/.thunderbird'.
-#   The script will looking for the old profile folder and will copy the
-#   folder into the new used profile folder.
+#   The script will looking for the old profile folder and will symlink the
+#   folder '${HOME}/.thunderbird' to the profile folder '${HOME}/.icedove'.
 #
 # Copyright:
 #   Licensed under the terms of GPLv2+.
@@ -22,20 +22,25 @@
 
 TB_HELPER="/usr/lib/thunderbird/thunderbird-wrapper-helper.sh"
 # sourcing external variables and helper functions
-if [ -f ${TB_HELPER} ]; then
-    . ${TB_HELPER}
+if [ -f "${TB_HELPER}" ]; then
+    . "${TB_HELPER}"
 else
     # this needs improving, the user isn't seen this error!
-    echo "helper ${TB_HELPER} not found!"
+    output_info "helper ${TB_HELPER} not found!"
     exit 1
 fi
 
 # some global variables
 MOZ_APP_NAME=thunderbird
-MOZ_APP_LAUNCHER=`which $0`
+MOZ_APP_LAUNCHER=$(which "$0")
 MOZ_LIBDIR=/usr/lib/${MOZ_APP_NAME}
 ID_PROFILE_FOLDER=${HOME}/.icedove
 TB_PROFILE_FOLDER=${HOME}/.thunderbird
+
+export HELP=0
+export FAIL=0
+export FORCE_MIMEAPPS_MIGRATE=0
+export VERBOSE=0
 
 # set MOZ_APP_LAUNCHER for gnome-session
 export MOZ_APP_LAUNCHER
@@ -46,6 +51,7 @@ while [ $# -gt 0 ]; do
     case ${ARG} in
         --fixmime)
             FIXMIME=1
+            FORCE_MIMEAPPS_MIGRATE=1
             ;;
         -g)
             DEBUGGER=1
@@ -65,7 +71,7 @@ while [ $# -gt 0 ]; do
             do_collect_backup_files
             exit 0
             ;;
-        --verbose) echo "[[ ... using verbose mode ... ]]"
+        --verbose) output_info "[[ ... using verbose mode ... ]]"
             VERBOSE=1
             ;;
         '?')
@@ -81,13 +87,14 @@ done
 
 # sanity check
 if [ "$DEBUGGER" != "" ] && [ "$USER_DEBUGGER" != "" ]; then
-    echo "You can't use option '-g and '-d' at the same time!"
+    output_info "You can't use option '-g and '-d' at the same time!"
     usage
     exit 1
 fi
 
 if [ "${FIXMIME}" = "1" ]; then
     do_fix_mimetypes_rdf
+    do_migrate_old_icedove_desktop
     do_collect_backup_files
     exit 0
 fi
@@ -116,9 +123,9 @@ fi
 # ${ID_PROFILE_FOLDER} and we can symlink to this.
 if [ -d "${ID_PROFILE_FOLDER}" -o -L "${ID_PROFILE_FOLDER}" ] && \
    [ ! -d "${TB_PROFILE_FOLDER}" -a ! -L "${TB_PROFILE_FOLDER}" ]; then
-    debug "found folder '${ID_PROFILE_FOLDER}'"
-    debug "not found folder or symlink '${TB_PROFILE_FOLDER}'"
-    debug "Start Thunderbird profile adoptions, please be patient!"
+    output_debug "found folder '${ID_PROFILE_FOLDER}'"
+    output_debug "not found folder or symlink '${TB_PROFILE_FOLDER}'"
+    output_debug "Start Thunderbird profile adoptions, please be patient!"
 
     # open a pop-up window with a message about starting migration
     do_inform_migration_start
@@ -134,72 +141,98 @@ if [ -d "${ID_PROFILE_FOLDER}" -o -L "${ID_PROFILE_FOLDER}" ] && \
     do_migrate_old_icedove_desktop
 
     # we are finished
-    debug "Thunderbird Profile adoptions done."
+    output_debug "Thunderbird Profile adoptions done."
     do_collect_backup_files
 fi
 
-# We found both profile folder,
+# We found both profile folder, and .thunderbird is a symlink,
 # we need to check if .thunderbird is symlinked to .icedove
 if [ -d "${ID_PROFILE_FOLDER}" -a -L "${TB_PROFILE_FOLDER}" ] && \
-   [ "$(/usr/bin/readlink ${TB_PROFILE_FOLDER})" != "${ID_PROFILE_FOLDER}" ];then
-    debug "Found folder ${ID_PROFILE_FOLDER} and a symlink ${TB_PROFILE_FOLDER} that is linked to ${ID_PROFILE_FOLDER}"
+   [ "$(/usr/bin/readlink -e "${TB_PROFILE_FOLDER}")" = "${ID_PROFILE_FOLDER}" ];then
 
-# ... or if .icedove is symlinked to .thunderbird
-elif [ -d "${TB_PROFILE_FOLDER}" -a -L "${ID_PROFILE_FOLDER}" ] && \
-     [ "$(/usr/bin/readlink ${ID_PROFILE_FOLDER})" != "${TB_PROFILE_FOLDER}" ];then
-    debug "Found folder ${TB_PROFILE_FOLDER} and a symlink ${ID_PROFILE_FOLDER} that is linked to ${TB_PROFILE_FOLDER}"
-    debug "You may want to remove the symlink ${ID_PROFILE_FOLDER}? It's probably not needed anymore."
+    output_debug "Found folder ${ID_PROFILE_FOLDER}, found a symlink ${TB_PROFILE_FOLDER} pointing to ${ID_PROFILE_FOLDER}"
 
-    if [ ! -f ${TB_PROFILE_FOLDER}/.migrated ]; then
-        # fixing mimeTypes.rdf which may have registered the iceweasel binary
+    # Check if we need to do some migration, the linking could be existing
+    # before we switched back to Thunderbird.
+    if [ ! -f "${TB_PROFILE_FOLDER}/.migrated" ]; then
+        # Fixing mimeTypes.rdf which may have registered the iceweasel binary
         # as browser, instead of x-www-browser
         do_fix_mimetypes_rdf
 
         # Fix local mimeapp.list and *.desktop entries
         do_migrate_old_icedove_desktop
-        /usr/bin/touch ${TB_PROFILE_FOLDER}/.migrated
+    fi
+
+# ... or the opposite if .icedove is symlinked to .thunderbird
+elif [ -d "${TB_PROFILE_FOLDER}" -a -L "${ID_PROFILE_FOLDER}" ] && \
+     [ "$(/usr/bin/readlink -e "${ID_PROFILE_FOLDER}")" != "${TB_PROFILE_FOLDER}" ];then
+
+    output_debug "Found folder ${TB_PROFILE_FOLDER}, found a symlink ${ID_PROFILE_FOLDER} pointing to ${TB_PROFILE_FOLDER}"
+    output_debug "You may want to remove the symlink ${ID_PROFILE_FOLDER}? It's probably not needed anymore."
+
+    # Check if we need to do some migration ...
+    if [ ! -f "${TB_PROFILE_FOLDER}/.migrated" ]; then
+        # Fixing mimeTypes.rdf which may have registered the iceweasel binary
+        # as browser, instead of x-www-browser
+        do_fix_mimetypes_rdf
+
+        # Fix local mimeapp.list and *.desktop entries
+        do_migrate_old_icedove_desktop
     fi
 
 # We found both profile folder, but they are not linked to each other! This
 # is a state we can't solve on our own !!! The user needs to interact and
-# has probably a old or otherwise used Thunderbird installation.
+# has probably a old or otherwise used Thunderbird installation. Which one
+# is the correct one to use?
 elif [ -d "${ID_PROFILE_FOLDER}" -o -L "${ID_PROFILE_FOLDER}" ] && \
      [ -d "${TB_PROFILE_FOLDER}" -o -L "${TB_PROFILE_FOLDER}" ] && \
-     [ "$(/usr/bin/readlink ${TB_PROFILE_FOLDER})" != "${ID_PROFILE_FOLDER}" ]; then
+     [ "$(/usr/bin/readlink -e "${TB_PROFILE_FOLDER}")" != "${ID_PROFILE_FOLDER}" ]; then
 
-    debug "There is already a folder '${TB_PROFILE_FOLDER}', will do nothing."
-    debug "Please investigate by yourself!"
-    logger -i -p warning -s "$0: [profile migration] Couldn't migrate Icedove into Thunderbird profile due existing folder '${TB_PROFILE_FOLDER}'!"
+    output_debug "There is already a folder or symlink '${TB_PROFILE_FOLDER}', will do nothing."
+    output_debug "Please investigate by yourself! Some more information below."
+    logger -i -p warning -s "$0: [profile migration] Couldn't migrate Icedove into Thunderbird profile due existing or symlinked folder '${TB_PROFILE_FOLDER}'!"
+    for CHECK in ${ID_PROFILE_FOLDER} ${TB_PROFILE_FOLDER}; do
+        FILE_CHECK=$(/usr/bin/readlink -e "${CHECK}")
+        if [ "${FILE_CHECK}" != "" ] && [ -L "${CHECK}" ]; then
+            output_debug "Found symlink '${FILE_CHECK}'"
+        elif [ "${FILE_CHECK}" != "" ] && [ -d "${CHECK}" ]; then
+            output_debug "Found folder '${FILE_CHECK}'"
+        else
+            output_debug "${CHECK} is probably a symlink pointing to a non existing target, at least not to ${ID_PROFILE_FOLDER}."
+            logger -i -p warning -s "$0: [profile migration] ${CHECK} is probably a symlink pointing to a non existing target, at least not to ${ID_PROFILE_FOLDER}."
+        fi
+    done
 
     # display a graphical advice if possible
     do_thunderbird2icedove_error_out
+
 fi
 
 if [ "$FAIL" = 1 ]; then
-    echo "A error happened while trying to migrate the old Icedove profile folder '${ID_PROFILE_FOLDER}'."
-    echo "Please take a look into the syslog file!"
+    output_info "A error happened while trying to migrate the old Icedove profile folder '${ID_PROFILE_FOLDER}'."
+    output_info "Please take a look into the syslog file!"
     exit 1
 fi
 
-# We have nothing to migrate or adopt, going further by starting Thunderbird.
+# If we are here we going simply further by starting Thunderbird.
 
 if [ "${DEBUG}" = "" ]; then
-    debug "call '$MOZ_LIBDIR/$MOZ_APP_NAME ${TB_ARGS}'"
-    $MOZ_LIBDIR/$MOZ_APP_NAME ${TB_ARGS}
+    output_debug "call '$MOZ_LIBDIR/$MOZ_APP_NAME ${TB_ARGS}'"
+    $MOZ_LIBDIR/$MOZ_APP_NAME "${TB_ARGS}"
 else
     # User has selected GDB?
     if [ "$DEBUGGER" = "1" ]; then
         # checking for GDB
         if [ -f /usr/bin/gdb ]; then
             if [ -f /usr/lib/debug/usr/lib/thunderbird/thunderbird ]; then
-                echo "Starting Thunderbird with GDB ..."
-                LANG= /usr/lib/thunderbird/run-mozilla.sh -g /usr/lib/thunderbird/thunderbird-bin "${TB_ARGS}"
+                output_info "Starting Thunderbird with GDB ..."
+                LANG='' /usr/lib/thunderbird/run-mozilla.sh -g /usr/lib/thunderbird/thunderbird-bin "${TB_ARGS}"
             else
-                echo "No package 'thunderbird-dbg' installed! Please install first and restart."
+                output_info "No package 'thunderbird-dbg' installed! Please install first and restart."
                 exit 1
             fi
         else
-            echo "No package 'gdb' installed! Please install first and try again."
+            output_info "No package 'gdb' installed! Please install first and try again."
             exit 1
         fi
     fi
